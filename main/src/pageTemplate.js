987379e51173eeda554b8f49661aa1f23dbfdb12
wyamet <wyattdomanski.md@gmail.com>
2023-02-09 15:47:07 -0800

    rough draft of pagetemplate

--- main/src/pageTemplate.js
+++ main/src/pageTemplate.js
@@ -10,9 +10,9 @@ const generateTeam = (team) => {
       <p class="card-text">${manager.officeNumber}</p>
     </div>
      `;
-   };
+  };
 
-   const generateEngineer = (engineer) => {
+  const generateEngineer = (engineer) => {
     return `
     <div class="card border-success mb-3" style="max-width: 18rem;">
     <div class="card-header bg-transparent border-success">Manager</div>
@@ -24,9 +24,9 @@ const generateTeam = (team) => {
     </div>
     
     `;
-};
+  };
 
-const generateIntern = (intern) => {
+  const generateIntern = (intern) => {
     return `
     <div class="card border-success mb-3" style="max-width: 18rem;">
     <div class="card-header bg-transparent border-success">Manager</div>
@@ -38,17 +38,44 @@ const generateIntern = (intern) => {
     </div>
     
     `;
+  };
+
+  const html = [];
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+  );
+  html.push(
+    team
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+  );
+  return html.join("");
 };
 
+module.exports = (team) => {
+  return `
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <h1>Hello, Team!</h1>
+    ${generateTeam(team)}
+  </body>
+</html>
+
+`;
 };
-
-
-
-
-
-
-
-
-
-
-const generateHTML = (html) => 
