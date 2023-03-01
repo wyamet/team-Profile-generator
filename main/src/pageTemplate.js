fdce0698515f3ac58b5eb171392341c812be85f3
wyamet <wyattdomanski.md@gmail.com>
2023-02-28 18:24:44 -0800

    updated css and html template

--- main/src/pageTemplate.js
+++ main/src/pageTemplate.js
@@ -59,14 +59,14 @@ const generateHTML = function (employees) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+      <link rel="stylesheet" href=""
       <title>My Team</title>
     </head>
     <body>
       <header>
         <h1>My Team</h1>
       </header>
-      <section>
+      <section class="wrapper">
         ${employees}
       </section>
     </body>
